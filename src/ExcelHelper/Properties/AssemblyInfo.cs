d6d63f5db97838347e3fcca4199c94ec032a0966
Kendall Bennett <kendallb@amain.com>
2017-10-20 22:49:43 -0700

    Updated to v1.1

--- src/ExcelHelper/Properties/AssemblyInfo.cs
+++ src/ExcelHelper/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("ExcelHelper.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010061c2ae4a301e71863e25862186c0b43c98a67be3ef92387c70b1ebb424d304858ec39680fce0bc0c9d0f3c5d18b0a7a6b3c7cde61e3367b89e5dfe3460e72e714ec77e752610eaf71afec81167ba6d160855866bf5183a88361e0bf289c1dcc140cbecaa80c3bdfd3a20402d3c295b6bbdd6c8d389317273c7ea66aa08e46ea9")]
